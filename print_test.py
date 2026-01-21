ce964e0a3e660f3c4f339eec95084ef99c463a1c
della-wrld <dsebastian3511@floridapoly.edu>
2026-01-21 02:18:13 +0000

    Updated print_test script

---
+++ print_test.py
@@ -0,0 +1,9 @@
+my_name = "Dellanely Sebastian"
+
+print(my_name)
+
+print("Tabs are replaced (from left to right) by one to eight spaces such that the total number of characters up to and including the replacement is a multiple of eight.")
+
+print("An expression statement evaluates the expression list")
+
+print("In this course, I hope to improve my knowledge about python, learn new features (to me) and improve my coding.")
